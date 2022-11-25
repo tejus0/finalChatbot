b898ad74ee3c8b24cfbb6a044a5d7803ffae183b
Tejus <tejuschaturvedi0@gmail.com>
2022-11-25 23:40:16 +0530

    images added

--- chatGui.py
+++ chatGui.py
@@ -1,13 +1,13 @@
-from tkinter import *
-from PIL import ImageTk,Image
-# import json
-import os
-import sys
-# imporVt cv2
 import json
+import os
+import random
 import re
-import random_responses
-
+import sys
+from tkinter import *
+import tkinter as tk
+import pyttsx3
+import random_responses as random_res
+from PIL import Image, ImageTk
 
 def load_json(file):
     with open(file) as bot_responses:
@@ -31,12 +31,12 @@ def get_response(input_string):
                 if word in required_words:
                     required_score += 1
 
-        # Amount of required words should match the required score
+        # Amount of required words should match the required scoreṇ
         if required_score == len(required_words):
             # print(required_score == len(required_words))
             # Check each word the user has typed
             for word in split_message:
-                # If the word is in the response, add to the score
+                # If the worṇd is in the response, add to the score
                 if word in response["user_input"]:
                     response_score += 1
 
@@ -56,8 +56,15 @@ def get_response(input_string):
     # If there is no good response, return a random one.
     if best_response != 0:
         return response_data[response_index]["bot_response"]
+    return random_res.random_string()
 
-    return random_responses.random_string()
+def textToSpeech(text):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+    engine.setProperty('rate', 150)
+    engine.say(text)
+    engine.runAndWait()
 
 def botReply():
     global question
@@ -65,72 +72,108 @@ def botReply():
     question = question.lower()
     # print(question)
     # input=question
-    answer=get_response(question)
+    answer = get_response(question)
+    textToSpeech(answer)
     # answer = bot.get_response(question)
     textarea.insert(END, "YOU:"+question+"\n\n")
     # textarea.insert(END, "Bot: "+str(answer)+'\n\n')
-    textarea.insert(END, "Bot: "+answer+'\n\n')
-    # pyttsx3.speak(answer)
+    textarea.insert(END, "Bot: "+str(answer)+'\n\n')
+    #engine = pyttsx3.init()
+    #engine.say('Hello sir, how may I help you, sir.')
+    #pyttsx3.speak("Hello,My name is Gautam")
     questionField.delete(0, END)
 
-
-root = Tk()
+root = tk.Tk()
 
 def img_bg1():
     # print(question)
     global img
-    if(question.find('library')!=-1):
-        print("yes")
-        image=Image.open('maps.jpg')
-        new_image = image.resize((800,800)) 
-        new_image.save('map1000.png')
-        img=PhotoImage(file= "map1000.png")
-    elif(question.find('admission')!=-1):
-        print("reached")
-        image=Image.open('pic.png')
-        new_image = image.resize((800,800)) 
-        new_image.save('map10001.png')
-        img=PhotoImage(file= "map10001.png")
-    elif(question.find('computer department')!=-1):
-        print("reached computer")
-        image=Image.open('ask.png')
-        new_image = image.resize((800,800)) 
-        new_image.save('map100001.png')
-        img=PhotoImage(file= "map100001.png")
-    elif(question.find('administration')!=-1):
-        print("reached administration")
-        image=Image.open('canteen.png')
-        new_image = image.resize((800,800)) 
-        new_image.save('map1000001.png')
-        img=PhotoImage(file= "map1000001.png")
-    elif(question.find('vc')!=-1):
-        print("reached vc")
-        image=Image.open('boysHostel.png')
-        new_image = image.resize((800,800)) 
-        new_image.save('map10000001.png')
-        img=PhotoImage(file= "map10000001.png")
+    if (question.find('library') != -1):
+        image = Image.open('library.png')
+        new_image = image.resize((800, 800))
+        new_image.save('library.png')
+        img = PhotoImage(file="library.png")
+    elif (question.find('electronics department') != -1):
+        image = Image.open('electronics_department.png')
+        new_image = image.resize((800, 800))
+        new_image.save('electronics_department.png')
+        img = PhotoImage(file="electronics_department.png")
+    elif (question.find('computer department') != -1):
+        image = Image.open('computer_department.png')
+        new_image = image.resize((800, 800))
+        new_image.save('computer_department.png')
+        img = PhotoImage(file="computer_department.png")
+    elif (question.find('administrative') != -1):
+        image = Image.open('administration.png')
+        new_image = image.resize((800, 800))
+        new_image.save('administration.png')
+        img = PhotoImage(file="administration.png")
+    elif (question.find('boys hostel') != -1):
+        image = Image.open('Boys_hostel.png')
+        new_image = image.resize((800, 800))
+        new_image.save('Boys_hostel.png')
+        img = PhotoImage(file="Boys_hostel.png")
+    elif (question.find('civil department') != -1):
+        image = Image.open('civil_department.png')
+        new_image = image.resize((800, 800))
+        new_image.save('civil_department.png')
+        img = PhotoImage(file="civil_department.png")
+    elif (question.find('mechanical department') != -1):
+        image = Image.open('mechanical_department.png')
+        new_image = image.resize((800, 800))
+        new_image.save('mechanical_department.png')
+        img = PhotoImage(file="mechanical_department.png")
+    elif (question.find('canteen') != -1):
+        image = Image.open('canteen.png')
+        new_image = image.resize((800, 800))
+        new_image.save('canteen.png')
+        img = PhotoImage(file="canteen.png")
+    elif (question.find('shakuntalam') != -1):
+        image = Image.open('shakuntalam.png')
+        new_image = image.resize((800, 800))
+        new_image.save('shakuntalam.png')
+        img = PhotoImage(file="shakuntalam.png")
+    elif (question.find('girls hostel') != -1):
+        image = Image.open('girls_hostel.png')
+        new_image = image.resize((800, 800))
+        new_image.save('girls_hostel.png')
+        img = PhotoImage(file="girls_hostel.png")
+    elif (question.find('dispensary') != -1):
+        image = Image.open('dispensary.png')
+        new_image = image.resize((800, 800))
+        new_image.save('dispensary.png')
+        img = PhotoImage(file="dispensary.png")
+    elif (question.find('vice chancellor') != -1):
+        image = Image.open('VC_OFFICE.png')
+        new_image = image.resize((800, 800))
+        new_image.save('VC_OFFICE.png')
+        img = PhotoImage(file="VC_OFFICE.png")
+    elif (question.find('playground') != -1):
+        image = Image.open('Playground.png')
+        new_image = image.resize((800, 800))
+        new_image.save('Playground.png')
+        img = PhotoImage(file="Playground.png")
 
 def newwindow():
-    newWindow = Toplevel(root) 
+    newWindow = Toplevel(root)
     newWindow.title("Maps of Departments")
     newWindow.geometry("800x800+100+30")
     img_bg1()
-    canvas1 = Canvas(newWindow, width = 400,height=400)
-    canvas1.pack(fill = "both", expand = True)
-    canvas1.create_image( 0, 0, image =img, anchor = "nw")# Display image
+    canvas1 = Canvas(newWindow, width=400, height=400)
+    canvas1.pack(fill="both", expand=True)
+    canvas1.create_image(0, 0, image=img, anchor="nw")  # Display image
     print(question)
-    # os.system('python bg.py')
 
 root.geometry('500x570+100+30')
 root.title('SAMARPAN CHATBOT')
-root.config(bg='deep pink')
+root.config(bg='brown')
 
-# logoPic = ImageTk.PhotoImage(file="samarpan_logo.png")
-""" logoPic=ImageTk.PhotoImage(Image.open('samarpan_logo.png').resize(50,100))
+image_logo = Image.open('logo.png')
+resize_image = image_logo.resize((120, 100))
+logoPic = ImageTk.PhotoImage(resize_image)
 
-logoPicLabel = Label(root, image=logoPic, bg='deep pink')
-logoPicLabel.image=logoPic
-logoPicLabel.pack(pady=5) """
+logoPicLabel = Label(root, image=logoPic, bg='black')
+logoPicLabel.pack(pady=5)
 
 centerFrame = Frame(root)
 centerFrame.pack()
@@ -139,22 +182,33 @@ scrollbar = Scrollbar(centerFrame)
 scrollbar.pack(side=RIGHT)
 
 textarea = Text(centerFrame, font=('times new roman', 20, 'bold'),
-                height=10, yscrollcommand=scrollbar.set, wrap='word')
+                height=5, yscrollcommand=scrollbar.set, wrap='word')
 textarea.pack(side=LEFT)
 scrollbar.config(command=textarea.yview)
 
-questionField = Entry(root, font=('verdana', 20, 'bold'))
-questionField.pack(pady=15, fill=X)
+def clear(event):
+   questionField.configure(state=NORMAL)
+   questionField.delete(0, END)
+   questionField.unbind('<Button-1>', clicked)
+
+prewrite=StringVar()
+questionField = Entry(root,textvariable=prewrite, font=('verdana', 20, 'bold'))
+questionField.insert(0,'Ask your question here .')
+questionField.pack(fill='x', expand=True, padx= 45, pady=10)
+clicked = questionField.bind('<Button-1>', clear)
+textarea.config()
 
 askPic = PhotoImage(file='ask.png')
-picPic=PhotoImage(file='pic.png')
+button_frame = Frame(root, height=10)
+button_frame.pack()
+askButton = Button(button_frame, text='Ask', command=botReply)
+askButton.pack(side=LEFT)
 
-askButton = Button(root, image=askPic, command=botReply)
-askButton.pack()
+direction = Button(button_frame, text='Locate on map', command=newwindow)
+direction.pack(side=RIGHT)
 
-direction = Button(root,image=picPic,command=newwindow)
-direction.pack(side='right')
 def click(event):
     askButton.invoke()
+
 root.bind('<Return>', click)
 root.mainloop()
